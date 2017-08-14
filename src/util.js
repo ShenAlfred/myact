bef038ddf7d055fbd427c1c522fa36107b249597
shenyuancheng <shenyuanchen@mail.xusenintl.com>
2017-08-14 15:39:32 +0800

    '时间处理方法修改'

--- src/util.js
+++ src/util.js
@@ -5,14 +5,34 @@ export default {
   isInteger: function (obj) {
     return parseInt(obj, 10) === obj
   },
-  handleTime: function(timestamp) {
+  /**
+   * 处理时间戳
+   * @param timestamp
+   * @returns {{}}
+   */
+  handleTime: function(timestamp, format) {
     var date = new Date(timestamp),
       result = {};
-    result.year = date.getFullYear();
-    result.month = date.getMonth()+1;
-    result.date = date.getDate();
-    result.hour = date.getHours();
-    result.min = date.getMinutes();
-    return result;
-  }
+    result["y+"] = date.getFullYear();
+    result["M+"] = date.getMonth()+1;
+    result["d+"] = date.getDate();
+    result["h+"] = date.getHours();
+    result["m+"] = date.getMinutes();
+    result["s+"] = date.getSeconds();
+
+    if(format)  {
+      if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+      }
+      for (var k in result) {
+        if (new RegExp("(" + k + ")").test(format)) {
+          format = format.replace(RegExp.$1, RegExp.$1.length == 1
+            ? result[k] : ("00" + result[k]).substr(("" + result[k]).length));
+        }
+      }
+      return format;
+    }else {
+      return result;
+    }
+  },
 }
